4ff9969d413d8900beef21fa51a5b2a3a520d05a
Oliver <oliver.ohara@student.manchester.ac.uk>
2020-03-06 17:08:33 +0000

    Add events now works without validation

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
 
 @Controller
 @RequestMapping(value = "/events", produces = { MediaType.TEXT_HTML_VALUE })
@@ -17,8 +20,8 @@ public class EventsController {
 	@Autowired
 	private EventService eventService;
 
-	//@Autowired
-	//private VenueService venueService;
+	@Autowired
+	private VenueService venueService;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String getAllEvents(Model model) {
@@ -32,10 +35,20 @@ public class EventsController {
 	@RequestMapping(value = "/add",method = RequestMethod.GET)
 	public String getAddEvents(Model model) {
 
-		model.addAttribute("events", eventService.findAll());
-		//model.addAttribute("venues", venueService.findAll());
+		model.addAttribute("event", new Event());
+		model.addAttribute("venues", venueService.findAll());
 
 		return "events/add/index";
+	}
+	
+	@RequestMapping(value="/add",method = RequestMethod.POST)
+	public String saveNewEvent(final Event event, final BindingResult bindingResult, final ModelMap model) {
+	    if (bindingResult.hasErrors()) {
+	        return "add";
+	    }
+	    this.eventService.save(event);
+	    model.clear();
+	    return "redirect:/events";
 	}
 
 }
--- src/main/java/uk/ac/man/cs/eventlite/entities/Event.java
+++ src/main/java/uk/ac/man/cs/eventlite/entities/Event.java
@@ -36,6 +36,8 @@ public class Event {
 	@ManyToOne
 	@JoinColumn(name = "Venue_id")
 	private Venue venue;
+	
+	private String description;
 
 	public Event() {
 	}
@@ -70,6 +72,14 @@ public class Event {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public Venue getVenue() {
