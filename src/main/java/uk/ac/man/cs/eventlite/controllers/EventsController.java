66d4c1dd61069a14e531b27a3cbe3fc77a466dfe
Shu-Ting Chang <shu-ting.chang@student.manchester.ac.uk>
2020-03-07 19:08:44 +0000

    Commented One-line Code
    Commented one line of code that was accidentally uncommented before,
    which caused two errors in the test.

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -41,7 +41,7 @@ public class EventsController {
 	public String getAllEvents(Model model) {
 
 		model.addAttribute("events", eventService.findAll());
-		model.addAttribute("venues", venueService.findAll());
+//		model.addAttribute("venues", venueService.findAll());
 
 		return "events/index";
 	}
