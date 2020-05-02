eb25004e0c47396a862819a01ddbdb23fcca7dc7
Shu-Ting <shu-ting.chang@student.manchester.ac.uk>
2020-05-02 15:17:40 +0100

    Event Attendee & Event Organiser
    - Changed Security.java to allow different actions for attendees and
    organisers.
    - Added <div sec:authorize="hasRole('ORGANISER')"> to web-pages that are
    related to the actions (only allowed for organisers).
    - Added links to static/index.html for convenience and slightly adjusted
    some web-pages' looks.

--- src/main/java/uk/ac/man/cs/eventlite/config/Security.java
+++ src/main/java/uk/ac/man/cs/eventlite/config/Security.java
@@ -18,18 +18,25 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 @EnableWebSecurity
 public class Security extends WebSecurityConfigurerAdapter {
 
-	public static final String ADMIN_ROLE = "ADMINISTRATOR";
+//	public static final String ADMIN_ROLE = "ADMINISTRATOR";
+	public static final String ATTENDEES_ROLE = "ATTENDEES";
+	public static final String ORGANISER_ROLE = "ORGANISER";
 
 	// List the mappings/methods for which no authorisation is required.
 	// By default we allow all GETs and full access to the H2 console.
 	private static final RequestMatcher[] NO_AUTH = { new AntPathRequestMatcher("/webjars/**", "GET"),
 			new AntPathRequestMatcher("/**", "GET"), new AntPathRequestMatcher("/h2-console/**") };
 
+	private static final RequestMatcher[] ORGANISER_AUTH = {new AntPathRequestMatcher("/events/update/**", "POST"),
+			new AntPathRequestMatcher("/events/add", "GET"), new AntPathRequestMatcher("/events/add", "POST"),
+			new AntPathRequestMatcher("/events/update/**", "GET"), new AntPathRequestMatcher("/**", "DELETE")};
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// By default, all requests are authenticated except our specific list.
-		http.authorizeRequests().requestMatchers(NO_AUTH).permitAll().anyRequest().hasRole(ADMIN_ROLE);
-
+		http.authorizeRequests().requestMatchers(NO_AUTH).permitAll().anyRequest().hasRole(ATTENDEES_ROLE);
+		http.authorizeRequests().requestMatchers(ORGANISER_AUTH).permitAll().anyRequest().hasRole(ORGANISER_ROLE);
+		
 		// Use form login/logout for the Web.
 		http.formLogin().loginPage("/sign-in").permitAll();
 		http.logout().logoutUrl("/sign-out").logoutSuccessUrl("/").permitAll();
@@ -50,12 +57,12 @@ public class Security extends WebSecurityConfigurerAdapter {
 	public UserDetailsService userDetailsService() {
 		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
-		UserDetails rob = User.withUsername("Rob").password(encoder.encode("Haines")).roles(ADMIN_ROLE).build();
-		UserDetails caroline = User.withUsername("Caroline").password(encoder.encode("Jay")).roles(ADMIN_ROLE).build();
-		UserDetails markel = User.withUsername("Markel").password(encoder.encode("Vigo")).roles(ADMIN_ROLE).build();
-		UserDetails mustafa = User.withUsername("Mustafa").password(encoder.encode("Mustafa")).roles(ADMIN_ROLE)
-				.build();
+		UserDetails rob = User.withUsername("Rob").password(encoder.encode("Haines")).roles(ATTENDEES_ROLE).build();
+		UserDetails caroline = User.withUsername("Caroline").password(encoder.encode("Jay")).roles(ATTENDEES_ROLE).build();
+		UserDetails markel = User.withUsername("Markel").password(encoder.encode("Vigo")).roles(ATTENDEES_ROLE).build();
+		UserDetails mustafa = User.withUsername("Mustafa").password(encoder.encode("Mustafa")).roles(ATTENDEES_ROLE).build();
+		UserDetails organiser = User.withUsername("Organiser").password(encoder.encode("Organiser")).roles(ORGANISER_ROLE).build();
 
-		return new InMemoryUserDetailsManager(rob, caroline, markel, mustafa);
+		return new InMemoryUserDetailsManager(rob, caroline, markel, mustafa, organiser);
 	}
 }
